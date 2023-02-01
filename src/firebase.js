1fea18bda54666b4bc4559622ae1c3528567ab64
Stepan Jamalyan <stepan.jamalyan@gmail.com>
2023-02-01 15:24:05 +0600

    base setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+   apiKey: 'AIzaSyAKM5CI1jCIhJyYeWakQ4eaYm4QQLPJmk0',
+   authDomain: 'netflix-clone-e323c.firebaseapp.com',
+   projectId: 'netflix-clone-e323c',
+   storageBucket: 'netflix-clone-e323c.appspot.com',
+   messagingSenderId: '259072030564',
+   appId: '1:259072030564:web:79897b1b2ed9fc5f292c71'
+}
